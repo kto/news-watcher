8d9f6655a7e2037635c2b7199b31daaf5517ea63
Kimmo Toro <kto@iki.fi>
2014-11-11 21:41:24 +0200

    Restructure, add NewsParser class

---
+++ newsparser/newsparser.py
@@ -0,0 +1,4 @@
+class NewsParser():
+	def __init__(self):
+		pass
+
--- tests/test_news_watch.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-class TestNewsWatch(unittest.TestCase):
-	def test_import(self):
-		try:
-			import news_watch
-		except ImportError as ie:
-			self.fail('Failed to import news_watch: {0}'.format(repr(ie)))
-
