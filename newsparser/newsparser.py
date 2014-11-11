bee3a991e91a1db42bee7e807ff5ecc3ea67303d
Kimmo Toro <kto@iki.fi>
2014-11-11 22:41:31 +0200

    Implement adding feeds.

--- newsparser/newsparser.py
+++ newsparser/newsparser.py
@@ -1,3 +1,10 @@
 class NewsParser():
     def __init__(self):
-        pass
+        self.feeds = []
+
+    def add_feed(self, feed):
+        if feed not in self.feeds:
+            self.feeds.append(feed)
+
+    def get_feeds(self):
+        return self.feeds
--- tests/test_newsparser.py
+++ tests/test_newsparser.py
@@ -2,6 +2,10 @@ import unittest
 
 
 class TestNewsParser(unittest.TestCase):
+    def get_newsparser(self):
+        from newsparser import newsparser
+        return newsparser.NewsParser()
+
     def test_import_module(self):
         try:
             import newsparser  # NOQA
@@ -13,3 +17,22 @@ class TestNewsParser(unittest.TestCase):
             from newsparser.newsparser import NewsParser  # NOQA
         except ImportError as ie:
             self.fail('Failed to import NewsParser class: {0}'.format(repr(ie)))
+
+    def test_add_feed_to_watch(self):
+        np = self.get_newsparser()
+        np.add_feed('http://test.feed')
+        self.assertTrue('http://test.feed' in np.get_feeds())
+        self.assertEquals(1, len(np.get_feeds()))
+
+    def test_add_multiple_feeds(self):
+        np = self.get_newsparser()
+        np.add_feed('http://test.feed')
+        np.add_feed('http://another.feed')
+        self.assertTrue('http://test.feed' in np.get_feeds())
+        self.assertTrue('http://another.feed' in np.get_feeds())
+
+    def test_adding_same_feed_twice(self):
+        np = self.get_newsparser()
+        np.add_feed('http://test.feed')
+        np.add_feed('http://test.feed')
+        self.assertEquals(1, len(np.get_feeds()))
