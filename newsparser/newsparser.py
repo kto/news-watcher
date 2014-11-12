9875317bbec34f3743e594edc6a4b98fc3fa8dd8
Kimmo Toro <kimmo.toro@f-secure.com>
2014-11-12 17:49:28 +0200

    Add feed parsing

--- newsparser/newsparser.py
+++ newsparser/newsparser.py
@@ -1,10 +1,27 @@
+import feedparser
+
+
 class NewsParser():
+    DEFAULT_CHECK_INTERVAL = 10
+
     def __init__(self):
         self.feeds = []
+        self.keywords = []
+        self.check_interval = self.DEFAULT_CHECK_INTERVAL
+        self.feedparser = feedparser
+        self.parsed_feeds = {}
 
     def add_feed(self, feed):
         if feed not in self.feeds:
             self.feeds.append(feed)
+            self.parsed_feeds.update({feed: self.feedparser.parse(feed)})
 
-    def get_feeds(self):
-        return self.feeds
+    def add_feeds(self, feeds):
+        for feed in feeds:
+            self.add_feed(feed)
+
+    def set_keywords(self, keywords):
+        self.keywords = keywords
+
+    def set_check_interval(self, interval):
+        self.check_interval = interval
--- tests/test_newsparser.py
+++ tests/test_newsparser.py
@@ -1,10 +1,14 @@
 import unittest
+import feedparser
+from mock import Mock
 
 
 class TestNewsParser(unittest.TestCase):
     def get_newsparser(self):
         from newsparser import newsparser
-        return newsparser.NewsParser()
+        np = newsparser.NewsParser()
+        np.feedparser = Mock(feedparser)
+        return np
 
     def test_import_module(self):
         try:
@@ -16,23 +20,66 @@ class TestNewsParser(unittest.TestCase):
         try:
             from newsparser.newsparser import NewsParser  # NOQA
         except ImportError as ie:
-            self.fail('Failed to import NewsParser class: {0}'.format(repr(ie)))
+            self.fail('Failed to import NewsParser class: {0}'.format(repr(ie)))  # NOQA
 
     def test_add_feed_to_watch(self):
         np = self.get_newsparser()
         np.add_feed('http://test.feed')
-        self.assertTrue('http://test.feed' in np.get_feeds())
-        self.assertEquals(1, len(np.get_feeds()))
+        self.assertEquals(1, len(np.feeds))
+        self.assertTrue('http://test.feed' in np.feeds)
 
     def test_add_multiple_feeds(self):
         np = self.get_newsparser()
         np.add_feed('http://test.feed')
         np.add_feed('http://another.feed')
-        self.assertTrue('http://test.feed' in np.get_feeds())
-        self.assertTrue('http://another.feed' in np.get_feeds())
+        self.assertTrue('http://test.feed' in np.feeds)
+        self.assertTrue('http://another.feed' in np.feeds)
 
     def test_adding_same_feed_twice(self):
         np = self.get_newsparser()
         np.add_feed('http://test.feed')
         np.add_feed('http://test.feed')
-        self.assertEquals(1, len(np.get_feeds()))
+        self.assertEquals(1, len(np.feeds))
+
+    def test_add_feeds(self):
+        np = self.get_newsparser()
+        test_feeds = ['http://test.feed', 'http://another.feed']
+        np.add_feeds(test_feeds)
+        self.assertEquals(2, len(np.feeds))
+        self.assertEquals(test_feeds, np.feeds)
+
+    def test_add_feeds_with_same_feed_multiple_times(self):
+        np = self.get_newsparser()
+        test_feeds = ['http://test.feed', 'http://test.feed']
+        np.add_feeds(test_feeds)
+        self.assertEquals(1, len(np.feeds))
+        self.assertEquals(['http://test.feed'], np.feeds)
+
+    def test_set_keywords(self):
+        np = self.get_newsparser()
+        keywords = ['foo', 'bar', 'bah', 'baz']
+        np.set_keywords(keywords)
+        self.assertEquals(keywords, np.keywords)
+
+    def test_set_empty_keywords_list(self):
+        np = self.get_newsparser()
+        np.set_keywords([])
+        self.assertEquals([], np.keywords)
+
+    def test_set_check_interval(self):
+        np = self.get_newsparser()
+        np.set_check_interval(5)
+        self.assertEquals(5, np.check_interval)
+
+    def test_default_check_interval(self):
+        np = self.get_newsparser()
+        self.assertEquals(np.DEFAULT_CHECK_INTERVAL, np.check_interval)
+
+    def test_feeds_parsed_after_added(self):
+        np = self.get_newsparser()
+        test_feed = {'link': 'http://test.feed'}
+        np.feedparser.parse = Mock(return_value=test_feed)
+        np.add_feed(test_feed['link'])
+        self.assertEquals(1, len(np.parsed_feeds))
+        self.assertEquals(test_feed, np.parsed_feeds[test_feed['link']])
+        np.feedparser.parse.assert_called_with(test_feed['link'])
